462d3e49e5a5fc503ab3d6ccef82c7900616c2ac
AriArch <rogerfiblasalvado@gmail.com>
2023-06-23 15:30:46 +0200

    Edit install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 echo "Installing requirements\n"
 pip install -r requirements.txt
@@ -9,19 +9,27 @@ mv main.py pm
 clear
 echo "Are you using zsh (1), bash (2), fish(3) or nu (4)"
 
+curdir="${pwd}"
+
 read -p ">> " input
 
-if [ "$input" == "1" ]; then
-    export PATH=".:$PATH"
-elif [ "$input" == "2" ]; then
-    export PATH=".:$PATH"
-elif [ "$input" == "3" ]; then
-    set -gx PATH . $PATH; source ~/.config/fish/config.fish
-elif [ "$input" == "4" ]; then
-    config set path $nu.path $nu.path:.
-else
-    true
+if [ "$input" = "1" ]; then
+    echo "alias pm=\"${curdir}/$(basename "$0")\"" >> ~/.zshrc
+    source ~/.zshrc
+
+elif [ "$input" = "2" ]; then
+    echo "alias pm=\"${curdir}/$(basename "$0")\"" >> ~/.bashrc
+    source ~/.bashrc
+
+elif [ "$input" = "3" ]; then
+    echo "function pm { ${curdir}/$(basename "$0"); }" >> ~/.config/fish/config.fish
+    source ~/.config/fish/config.fish
+
+elif [ "$input" = "4" ]; then
+    echo "config set path \$nu.path \$nu.path:${curdir}" >> ~/.config/nu/config.toml
+    echo "Restart your terminal"
 fi
+
 
 echo ""
 echo "All done."
